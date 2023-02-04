1c2d102aec16db9fb6c96f290c8697acea482cde
kakandavorever <70788217+kmmaniez@users.noreply.github.com>
2023-02-04 08:19:01 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { Configuration, OpenAIApi } from 'openai'
 dotenv.config()
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
